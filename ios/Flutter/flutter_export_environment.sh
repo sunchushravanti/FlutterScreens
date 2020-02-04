140e2462f2f46cbe7fb460f9f77ad601fdfae047
Ajit Mohite <amohite@LITL.LOCAL>
2020-02-04 15:08:08 +0530

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/Lauren/Documents/Shravanti/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Lauren/Desktop/FlutterScreens-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Lauren/Documents/Shravanti/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
